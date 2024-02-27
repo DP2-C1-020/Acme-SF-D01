b4ded24cc0ec4cebc8ded38802b1706e716b1c8d
gabrielvcg <gabrivacaro@gmail.com>
2024-02-27 22:46:39 +0100

    Student 1-feature013 implemented Manager role

---
+++ src/main/java/acme/roles/Manager.java
@@ -0,0 +1,39 @@
+
+package acme.roles;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.client.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Manager extends AbstractRole {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				degree;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				overview;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				certifications;
+
+	@Column(nullable = true)
+	private String				link;
+
+}
