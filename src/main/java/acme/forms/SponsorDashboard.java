fc49362cd0fa0547af60b5640ab5a55082eb3837
diegonqui <digonquin@gmail.com>
2024-03-08 17:45:59 +0100

    Student4 Feature 4 completed

---
+++ src/main/java/acme/forms/SponsorDashboard.java
@@ -0,0 +1,32 @@
+
+package acme.forms;
+
+import acme.client.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SponsorDashboard extends AbstractForm {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	private int					totalInvoiceHasTaxLessEqual21;
+
+	private int					totalSponsorshipHasLink;
+
+	private double				avgAmountSponsorship;
+	private double				devAmountSponsorship;
+	private double				minAmountSponsorship;
+	private double				maxAmountSponsorship;
+
+	private double				avgQuantityInvoice;
+	private double				devQuantityInvoice;
+	private double				minQuantityInvoice;
+	private double				maxQuantityInvoice;
+
+}
