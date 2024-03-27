4e4bcb394f079e48c961780594ce26799e665ff8
elguille2 <guillermo2pacheco@gmail.com>
2024-03-27 09:45:34 +0100

    List and Show Objectives

---
+++ src/main/java/acme/features/authenticated/objectives/AuthenticatedObjectiveShowService.java
@@ -0,0 +1,58 @@
+
+package acme.features.authenticated.objectives;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Authenticated;
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.client.views.SelectChoices;
+import acme.entities.objectives.Objective;
+import acme.entities.objectives.PriorityType;
+
+@Service
+public class AuthenticatedObjectiveShowService extends AbstractService<Authenticated, Objective> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AuthenticatedObjectiveRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Objective object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findOneObjectiveById(id);
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void unbind(final Objective object) {
+		assert object != null;
+
+		SelectChoices choices;
+		Dataset dataset;
+
+		choices = SelectChoices.from(PriorityType.class, object.getPriority());
+
+		dataset = super.unbind(object, "instantiationMoment", "title", "description", "priority", "status", "startDate", "endDate", "link");
+		dataset.put("confirmation", false);
+		dataset.put("readonly", true);
+		dataset.put("statuses", choices);
+
+		super.getResponse().addData(dataset);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/authenticated/objective/form.jsp
@@ -0,0 +1,20 @@
+<%@page%>
+
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="acme" uri="http://acme-framework.org/"%>
+
+<acme:form readonly="${readonly}">
+<acme:input-textbox code="authenticated.objective.form.label.instantiationMoment" path="instantiationMoment"/>  
+    <acme:input-textbox code="authenticated.objective.form.label.title" path="title"/>  
+    <acme:input-textarea code="authenticated.objective.form.label.description" path="description"/>  
+    <acme:input-select code="authenticated.objective.form.label.priority" path="priority" choices="${statuses}"/>  
+    <acme:input-textbox code="authenticated.objective.form.label.status" path="status"/>
+    <acme:input-textbox code="authenticated.objective.form.label.startDate" path="startDate"/>  
+    <acme:input-textbox code="authenticated.objective.form.label.enDate" path="endDate"/>  
+    <acme:input-url code="authenticated.objective.form.label.link" path="link"/>  
+	
+	<jstl:if test="${!readonly}">
+		<acme:input-checkbox code="authenticated.objective.form.label.confirmation" path="confirmation"/>
+		<acme:submit code="authenticated.objective.form.button.create" action="/authenticated/objective/create"/>
+	</jstl:if>
+</acme:form>
