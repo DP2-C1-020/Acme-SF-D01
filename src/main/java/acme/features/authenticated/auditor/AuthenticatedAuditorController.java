e3dc9be8bf4796066b312705eb0498add45673ff
elguille2 <guillermo2pacheco@gmail.com>
2024-04-25 11:34:41 +0200

    Add student 5 features 17 and 18

---
+++ src/main/java/acme/features/authenticated/auditor/AuthenticatedAuditorController.java
@@ -0,0 +1,33 @@
+
+package acme.features.authenticated.auditor;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.client.controllers.AbstractController;
+import acme.client.data.accounts.Authenticated;
+import acme.roles.Auditor;
+
+@Controller
+public class AuthenticatedAuditorController extends AbstractController<Authenticated, Auditor> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AuthenticatedAuditorCreateService	createService;
+
+	@Autowired
+	protected AuthenticatedAuditorUpdateService	updateService;
+
+	// Contructors ------------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("create", this.createService);
+		super.addBasicCommand("update", this.updateService);
+	}
+
+}
