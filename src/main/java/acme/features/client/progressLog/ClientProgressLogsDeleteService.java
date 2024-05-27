5727b7932ae9a8b6145e5ba8dbc2def9e6e0be5f
alvcarber1 <acarrerabernal@gmail.com>
2024-05-27 20:01:45 +0200

    Docs student2

--- src/main/java/acme/features/client/progressLog/ClientProgressLogsDeleteService.java
+++ src/main/java/acme/features/client/progressLog/ClientProgressLogsDeleteService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.client.data.accounts.Principal;
-import acme.client.data.models.Dataset;
 import acme.client.services.AbstractService;
 import acme.entities.contracts.Contract;
 import acme.entities.progress_logs.ProgressLog;
@@ -72,13 +71,5 @@ public class ClientProgressLogsDeleteService extends AbstractService<Client, Pro
 	@Override
 	public void unbind(final ProgressLog object) {
 		assert object != null;
-
-		Dataset dataset;
-		Contract objectContract = object.getContract();
-
-		dataset = super.unbind(object, "recordId", "completeness", "comment", "registrationMoment", "responsiblePerson", "draftMode");
-		dataset.put("contractCode", objectContract.getCode());
-
-		super.getResponse().addData(dataset);
 	}
 }
