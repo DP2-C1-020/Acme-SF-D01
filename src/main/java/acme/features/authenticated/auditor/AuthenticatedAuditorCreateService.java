71b18f94bfd4ef2779652a5bbb35579dab5efdb3
elguille2 <guillermo2pacheco@gmail.com>
2024-10-04 10:03:42 +0200

    Fix bug when becoming auditor

--- src/main/java/acme/features/authenticated/auditor/AuthenticatedAuditorCreateService.java
+++ src/main/java/acme/features/authenticated/auditor/AuthenticatedAuditorCreateService.java
@@ -59,10 +59,18 @@ public class AuthenticatedAuditorCreateService extends AbstractService<Authentic
 	@Override
 	public void validate(final Auditor object) {
 		assert object != null;
+
+		if (!super.getBuffer().getErrors().hasErrors("professionalID")) {
+			Auditor isCodeUnique;
+			isCodeUnique = this.repository.findAuditorByCode(object.getProfessionalID());
+			super.state(isCodeUnique == null, "professionalID", "validation.auditor.code.duplicate");
+		}
+
 	}
 
 	@Override
 	public void perform(final Auditor object) {
+		assert object != null;
 		this.repository.save(object);
 	}
 
